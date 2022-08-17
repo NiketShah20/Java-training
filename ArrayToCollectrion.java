c5e42d95acfe45965df580295643d41d3004093a
NiketShah20 <109204906+NiketShah20@users.noreply.github.com>
2022-08-17 12:07:55 +0530

    Update ArrayToCollectrion.java

--- ArrayToCollectrion.java
+++ ArrayToCollectrion.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class ArrayToCollectrion {
 	public static void main(String[] args) {
 	
-		Integer [] ar1= {1,2,3,4,5,6,6,32,11,1334,};
-		ArrayList<Integer> List1=new ArrayList<>();
+		Integer [] ar1= {1,2,3,4,5,6,6,32,11,1334};
+		ArrayList<Integer> List1=new ArrayList<Integer>();
+		for(int i=0;i<ar1.length;i++) {
+			List1.add(ar1[i]);
+		}
 		System.out.println(List1);
 	}
 
